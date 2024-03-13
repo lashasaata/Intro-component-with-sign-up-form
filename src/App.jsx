d119c42774e0bd9efa3399ae9eebeaa737acea32
lashasaata <lashasaata@gmail.com>
2024-03-13 12:51:39 -0700

    modified app.jsx where are added components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import Description from "./components/Description";
+import Signform from "./components/Signform";
 
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center gap-[64px] bg-[url('./images/bg-intro-mobile.png')] bg-red-400">
+      <Description />
+      <main className=" flex flex-col gap-[24px]">
+        <Signform />
+      </main>
+    </div>
+  );
 }
 
 export default App;
