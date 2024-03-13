2fa40c239d5a2a86d75993d0dc0b304c946e2a5d
lashasaata <lashasaata@gmail.com>
2024-03-13 12:48:31 -0700

    description part's mobile styles and elements

---
+++ src/components/Description.jsx
@@ -0,0 +1,18 @@
+function Description() {
+  return (
+    <div className="w-[327px] flex flex-col items-center gap-[16px]">
+      <h1 className="text-white text-[28px] leading-[1.29] tracking-[-0.29px] font-bold text-center">
+        Learn to code by <br /> watching others
+      </h1>
+      <p className="text-white text-base font-medium leading-[1.63] text-center">
+        See how experienced developers solve <br /> problems in real-time.
+        Watching <br />
+        scripted tutorials is great, but <br /> understanding how developers
+        think is <br />
+        invaluable.
+      </p>
+    </div>
+  );
+}
+
+export default Description;
