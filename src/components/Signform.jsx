4726fdd93a583c60237fa9762647d05d6b47902e
lashasaata <lashasaata@gmail.com>
2024-03-14 11:43:14 -0700

    add shadows and styled input parts

--- src/components/Signform.jsx
+++ src/components/Signform.jsx
@@ -1,7 +1,7 @@
 function Signform() {
   return (
     <>
-      <section className="w-[327px] h-[88px] rounded-[10px] bg-[#5e54a4] flex items-center justify-center">
+      <section className="w-[327px] h-[88px] rounded-[10px] bg-[#5e54a4] flex items-center justify-center shadow-section">
         <p className="text-center text-white text-[15px] leading-[26px] tracking-[0.27px] font-medium">
           <span className="font-bold">Try it free 7 days </span>
           then <br /> $20/mo. thereafter
@@ -9,37 +9,57 @@ function Signform() {
       </section>
       <form
         action=""
-        className="flex flex-col items-center gap-[16px] p-6 bg-white rounded-[10px] shadow-[0px_8px_0px_0px_rgba(0, 0, 0, 0.15)]"
+        className="flex flex-col items-center gap-[16px] p-6 bg-white rounded-[10px] shadow-section"
       >
-        <div>
-          <div className="flex fustify-between items-center">
-            <input name="firstName" type="text" placeholder="First Name" />
+        <div className="flex flex-col items-end">
+          <div className="flex justify-between items-center w-[279px] h-[56px] border border-solid border-[#dedede] px-5 rounded-[5px] ">
+            <input
+              name="firstName"
+              type="text"
+              placeholder="First Name"
+              className="text-sm text-[#3d3b48] leading-[1.89] tracking-[0.25px] font-[600] focus:outline-none focus:caret-[#5e54a4] placeholder:text-[#3d3b48] placeholder:text-sm placeholder:leading-[1.86] placeholder:tracking-[0.25px] placeholder:font-[600]"
+            />
             <img src="./images/icon-error.svg" alt="error icon" />
           </div>
-          <p></p>
+          <p className="text-[11px] text-[#ff7979] font-[500]">ngsasa dga</p>
         </div>
         <div>
-          <div className="flex fustify-between items-center">
-            <input name="lastName" type="text" placeholder="Last Name" />
+          <div className="flex justify-between items-center w-[279px] h-[56px] border border-solid border-[#dedede] px-5">
+            <input
+              name="lastName"
+              type="text"
+              placeholder="Last Name"
+              className="text-sm text-[#3d3b48] leading-[1.89] tracking-[0.25px] font-[600] focus:outline-none focus:caret-[#5e54a4] placeholder:text-[#3d3b48] placeholder:text-sm placeholder:leading-[1.86] placeholder:tracking-[0.25px] placeholder:font-[600]"
+            />
             <img src="./images/icon-error.svg" alt="error icon" />
           </div>
-          <p></p>
+          <p className="text-[11px] text-[#ff7979] font-[500]"></p>
         </div>
         <div>
-          <div className="flex fustify-between items-center">
-            <input name="email" type="email" placeholder="Email Address" />
+          <div className="flex justify-between items-center w-[279px] h-[56px] border border-solid border-[#dedede] px-5">
+            <input
+              name="email"
+              type="email"
+              placeholder="Email Address"
+              className="text-sm text-[#3d3b48] leading-[1.89] tracking-[0.25px] font-[600] focus:outline-none focus:caret-[#5e54a4] placeholder:text-[#3d3b48] placeholder:text-sm placeholder:leading-[1.86] placeholder:tracking-[0.25px] placeholder:font-[600]"
+            />
             <img src="./images/icon-error.svg" alt="error icon" />
           </div>
-          <p></p>
+          <p className="text-[11px] text-[#ff7979] font-[500]"></p>
         </div>
         <div>
-          <div className="flex fustify-between items-center">
-            <input name="password" type="password" placeholder="Password" />
+          <div className="flex justify-between items-center w-[279px] h-[56px] border border-solid border-[#dedede] px-5">
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-[#3d3b48] leading-[1.89] tracking-[0.25px] font-[600] focus:outline-none focus:caret-[#5e54a4] placeholder:text-[#3d3b48] placeholder:text-sm placeholder:leading-[1.86] placeholder:tracking-[0.25px] placeholder:font-[600]"
+            />
             <img src="./images/icon-error.svg" alt="error icon" />
           </div>
-          <p></p>
+          <p className="text-[11px] text-[#ff7979] font-[500]"></p>
         </div>
-        <button className="w-[279px] h-[56px] rounded-[5px] bg-[#38cc8b] text-white text-[15px] font-[600] leading-[1.73] tracking-[1px]">
+        <button className="w-[279px] h-[56px] rounded-[5px] bg-[#38cc8b] text-white text-[15px] font-[600] leading-[1.73] tracking-[1px] shadow-button">
           CLAIM YOUR FREE TRIAL
         </button>
         <p className="text-[11px] leading-[21px] leading-[1.91] font-[500] text-[#bab7d4] text-center mt-[-8px]">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    boxShadow: {
+      section: "0 8px 0 0 rgba(0, 0, 0, 0.15)",
+      button: "inset 0 -4px 0 0 rgba(0, 0, 0, 0.09);",
+    },
   },
   plugins: [],
 };
